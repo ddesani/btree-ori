4cac644e67d5a3cb3eedbab3693e0f18701114ff
victorbrsantos <victorbrsantos@53171014-be3d-11dd-824f-6f77523b18d3>
2008-11-29 18:42:04 +0000

    Classe Arquivo

---
+++ BTree/Arquivo.cpp
@@ -0,0 +1,13 @@
+/*
+ * Arquivo.cpp
+ *
+ *  Created on: 29/11/2008
+ *      Author: victor
+ */
+
+#include "Arquivo.h"
+
+Arquivo::Arquivo() {
+	// TODO Auto-generated constructor stub
+
+}
---
+++ BTree/Arquivo.h
@@ -0,0 +1,9 @@
+class Arquivo
+{
+	private:
+
+	public:
+		Arquivo();
+		~Arquivo();
+};
+
