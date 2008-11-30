e7dbaf2ad98cb02b3a1a1b56d13c3f4cb655ef03
ddesani <ddesani@53171014-be3d-11dd-824f-6f77523b18d3>
2008-11-30 18:55:47 +0000

    Class Arquivo Class Registro 	ajusta 	getRA Obs: Todos os 100 registros foram lidos corretamente.

--- BTree/src/Registro.h
+++ BTree/src/Registro.h
@@ -19,25 +19,29 @@ public:
 	char telefone2[11];
 	char curso[25];
 	char anoIngresso[4];
-	
+	//char	reg[232];
 		
 public:
-	int getRA(char* ra, int n)
+	
+	
+	int getRA()
 	{
-		return atoi(ajusta(ra,7));
+		return atoi(ajusta(RA,7));
 	}
 	
 	char* ajusta(char* var, int n)
 	{
 		char* aux = new char [n-1];
 		
-		for(int i=0; i<n; ++i)
-			aux[i] = '\0';
 		
-		for(int i=0; i<n-1; ++i)
+		for(int i=0; i<(n-1); ++i)
 			aux[i] = var[i];
+		//strncpy(aux,var,n-1);
 		
 		strcpy(var,aux);
+		
+		for(int i=0; i<(n-1); ++i)
+			aux[i] = '\0';
 		
 		delete[] aux;
 
@@ -63,7 +67,31 @@ public:
 		cout << "CEP S: " << ajusta(CEP2,9) << endl;
 		cout << "Telefone S: " << ajusta(telefone2,11) << endl;
 		cout << "Curso: " << ajusta(curso,25) << endl;
+		cout << "Ano Ingresso: " << ajusta(anoIngresso,5) << endl;
+		cout << endl;
+	}
+	void imprime2()
+	{
+		cout << endl;
+		cout << "Registro de Alunos" << endl;
+		cout << "Nome: " << nome << endl;
+		cout << "RG: " << RG << endl;
+		cout << "CPF: " << CPF << endl;
+		cout << "RA: " << RA << endl;
+		cout << "Cidade: " << cidade << endl;
+		cout << "Rua: " << rua << endl;
+		cout << "Numero: " << numero << endl;
+		cout << "CEP: " << CEP << endl;
+		cout << "UF: " << UF << endl;
+		cout << "Telefone: " << telefone << endl;
+		cout << "Rua S: " << rua2<< endl;
+		cout << "Numero S: " << numero2 << endl;
+		cout << "CEP S: " << CEP2 << endl;
+		cout << "Telefone S: " << telefone2 << endl;
+		cout << "Curso: " << curso << endl;
 		cout << "Ano Ingresso: " << anoIngresso << endl;
 		cout << endl;
 	}
+
+	
 };
