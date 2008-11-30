705f028b477fa199ca8ad3ec3669f7a025d4e93c
ddesani <ddesani@53171014-be3d-11dd-824f-6f77523b18d3>
2008-11-30 21:58:40 +0000

    Ultimo commit - Sabado 	tentar fazer a escrita

--- trunk/BTree/src/Registro.h
+++ trunk/BTree/src/Registro.h
@@ -20,57 +20,84 @@ public:
 	char curso[25];
 	char anoIngresso[4];
 	//char	reg[232];
-		
-public:
-	
-	
+
+
 	int getRA()
 	{
-		return atoi(ajusta(RA,7));
+		return atoi(RA);
 	}
-	
-	char* ajusta(char* var, int n)
-	{
-		char* aux = new char [n-1];
-		
-		
-		for(int i=0; i<(n-1); ++i)
-			aux[i] = var[i];
-		//strncpy(aux,var,n-1);
-		
-		strcpy(var,aux);
-		
-		for(int i=0; i<(n-1); ++i)
-			aux[i] = '\0';
-		
-		delete[] aux;
 
-		return var;
+	void ajustaI()
+	{
+		ajustaI(nome, 40);
+		ajustaI(RG, 10);
+		ajustaI(CPF, 12);
+		ajustaI(RA, 7);
+		ajustaI(cidade, 20);
+		ajustaI(rua, 25);
+		ajustaI(numero, 5);
+		ajustaI(CEP, 9);
+		ajustaI(UF, 3);
+		ajustaI(telefone, 11);
+		ajustaI(rua2, 35);
+		ajustaI(numero2, 5);
+		ajustaI(CEP2, 9);
+		ajustaI(telefone2, 11);
+		ajustaI(curso, 25);
+		ajustaI(anoIngresso, 5);
+		str[0]=nome[0];
+
+	}
+	void init()
+	{
+		strncat(str,nome,40);
+		strncat(str,RG,10);
+		strncat(str,CPF,12);
+		strncat(str,RA,7);
+		strncat(str,cidade,20);
+		strncat(str,rua,35);
+		strncat(str,numero,5);
+		strncat(str,CEP,9);
+		strncat(str,UF,3);
+		strncat(str,telefone,11);
+		strncat(str,rua2,35);
+		strncat(str,numero2,5);
+		strncat(str,CEP2,9);
+		strncat(str,telefone2,11);
+		strncat(str,curso,25);
+		strncat(str,anoIngresso,4);
+		str[241]='\n';
+		
 	}
 	
-	void imprime()
+	char*	getStr()
+	{
+		return str;
+	}
+
+	/*void imprime()
 	{
 		cout << endl;
 		cout << "Registro de Alunos" << endl;
-		cout << "Nome: " << ajusta(nome,40) << endl;
-		cout << "RG: " << ajusta(RG,10) << endl;
-		cout << "CPF: " << ajusta(CPF,12) << endl;
-		cout << "RA: " << ajusta(RA,7) << endl;
-		cout << "Cidade: " << ajusta(cidade,20) << endl;
-		cout << "Rua: " << ajusta(rua,25) << endl;
-		cout << "Numero: " << ajusta(numero,5) << endl;
-		cout << "CEP: " << ajusta(CEP,9) << endl;
-		cout << "UF: " << ajusta(UF,3) << endl;
-		cout << "Telefone: " << ajusta(telefone,11) << endl;
-		cout << "Rua S: " << ajusta(rua2,35) << endl;
-		cout << "Numero S: " << ajusta(numero2,5) << endl;
-		cout << "CEP S: " << ajusta(CEP2,9) << endl;
-		cout << "Telefone S: " << ajusta(telefone2,11) << endl;
-		cout << "Curso: " << ajusta(curso,25) << endl;
-		cout << "Ano Ingresso: " << ajusta(anoIngresso,5) << endl;
+		cout << "Nome: " << ajustaI(nome, 40) << endl;
+		cout << "RG: " << ajustaI(RG, 10) << endl;
+		cout << "CPF: " << ajustaI(CPF, 12) << endl;
+		cout << "RA: " << ajustaI(RA, 7) << endl;
+		cout << "Cidade: " << ajustaI(cidade, 20) << endl;
+		cout << "Rua: " << ajustaI(rua, 25) << endl;
+		cout << "Numero: " << ajustaI(numero, 5) << endl;
+		cout << "CEP: " << ajustaI(CEP, 9) << endl;
+		cout << "UF: " << ajustaI(UF, 3) << endl;
+		cout << "Telefone: " << ajustaI(telefone, 11) << endl;
+		cout << "Rua S: " << ajustaI(rua2, 35) << endl;
+		cout << "Numero S: " << ajustaI(numero2, 5) << endl;
+		cout << "CEP S: " << ajustaI(CEP2, 9) << endl;
+		cout << "Telefone S: " << ajustaI(telefone2, 11) << endl;
+		cout << "Curso: " << ajustaI(curso, 25) << endl;
+		cout << "Ano Ingresso: " << ajustaI(anoIngresso, 5) << endl;
 		cout << endl;
-	}
-	void imprime2()
+	}*/
+	void imprime()
 	{
 		cout << endl;
 		cout << "Registro de Alunos" << endl;
@@ -93,5 +120,26 @@ public:
 		cout << endl;
 	}
 
+private:
+
+	char	str[242];
 	
+	char* ajustaI(char* var, int n)
+	{
+		char* aux = new char [n-1];
+
+		for (int i=0; i<(n-1); ++i)
+			aux[i] = var[i];
+		//strncpy(aux,var,n-1);
+
+		strcpy(var, aux);
+
+		for (int i=0; i<(n-1); ++i)
+			aux[i] = '\0';
+
+		delete[] aux;
+
+		return var;
+	}
+
 };
