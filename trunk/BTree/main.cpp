e2d979f0e2d246bf48b46d6aad8b6f401d0ccaed
victorbrsantos <victorbrsantos@53171014-be3d-11dd-824f-6f77523b18d3>
2008-12-02 01:56:48 +0000

    Classe Arquivo 	insereArquivo()

--- trunk/BTree/main.cpp
+++ trunk/BTree/main.cpp
@@ -6,35 +6,14 @@ using namespace std;
 #include "src/Registro.h"
 #include "src/Arquivo.h"
 
-
 int main()
 {
     Arquivo objArq("registro.txt");
-   
-    /*char nome[242];
-
-     
-
-      int dim[16];
-      dim[0]= 40;
-      dim[1]= 10;
-      dim[2]= 12;
-      dim[3]= 7;
-      dim[4]= 20;
-      dim[5]= 35;
-      dim[6]= 5;
-      dim[7]= 9;
-      dim[8]= 3;
-      dim[9]= 11;
-      dim[10]= 35;
-      dim[11]= 5;
-      dim[12]= 9;
-      dim[13]= 11;
-      dim[14]= 25;
-      dim[15]= 4;*/
-
+	
+    //objArq.inserirArq();
+	
       
 
-      cout << endl <<  "Fim" << endl;
-      return 0;
+    cout << endl <<  "Fim" << endl;
+    return 0;
 }
--- trunk/BTree/src/Arquivo.h
+++ trunk/BTree/src/Arquivo.h
@@ -15,11 +15,155 @@ public:
 		
 		f.seekg(0*242,ios::beg);
 		f.getline(reinterpret_cast<char *>(&r), 242);
-		f.seekg(100*242,ios::beg);
-		f.write(reinterpret_cast<char *>(&r), 242);
-		f << '\xA';
+		r.ajusta();
+		r.imprime();
+		//f.seekg(100*242,ios::beg);
+		//f.write(reinterpret_cast<char *>(&r), 242);
+		//f << '\xA';
 				
 	}
+	
+	
+	//insere espacos vazios no fim de cada variavel char
+	char* insereEspaco(char *a, int tamanho) {
+		char *aux = new char[tamanho];
+		int i = 0;		
+		while (a[i] != '\0' && i != tamanho - 1) {
+			aux[i] = a[i];
+			++i;
+		}
+		while (i < tamanho - 1)
+			aux[i++] = ' ';
+		
+		aux[i] = '\0';	
+		return aux;
+	}
+	
+	void inserirArq(int n)
+	{
+		//Tipo registro que auxilia com as variaveis para formar o vetor de char do registro
+		Registro reg; 
+		
+		//Registro final com todos os campos
+		char registro[242];
+		
+		//nome
+		cout << "Nome: ";
+		cin >> reg.nome;
+		char *aux = insereEspaco(reg.nome, reg.tamanhoNome);
+		strcpy(registro, aux);
+		strcat(registro, " ");
+		//RG
+		cout << "RG: ";
+		cin >> reg.RG;
+		aux = insereEspaco(reg.RG, reg.tamanhoRG);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//CPF
+		cout << "CPF: ";
+		cin >> reg.CPF;
+		aux = insereEspaco(reg.CPF, reg.tamanhoCPF);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//RA
+		cout << "RA: ";
+		cin >> reg.RA;
+		aux = insereEspaco(reg.RA, reg.tamanhoRA);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Cidade
+		cout << "Cidade: ";
+		cin >> reg.cidade;
+		aux = insereEspaco(reg.cidade, reg.tamanhoCidade);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Rua
+		cout << "Rua: ";
+		cin >> reg.rua;
+		aux = insereEspaco(reg.rua, reg.tamanhoRua);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Numero
+		cout << "Numero: ";
+		cin >> reg.numero;
+		aux = insereEspaco(reg.numero, reg.tamanhoNumero);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//CEP
+		cout << "CEP: ";
+		cin >> reg.CEP;
+		aux = insereEspaco(reg.CEP, reg.tamanhoCEP);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//UF
+		cout << "UF: ";
+		cin >> reg.UF;
+		aux = insereEspaco(reg.UF, reg.tamanhoUF);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Telefone
+		cout << "Telefone: ";
+		cin >> reg.telefone;
+		aux = insereEspaco(reg.telefone, reg.tamanhoTelefone);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Rua 2
+		cout << "Rua 2: ";
+		cin >> reg.rua2;
+		aux = insereEspaco(reg.rua2, reg.tamanhoRua2);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Numero 2
+		cout << "Numero 2: ";
+		cin >> reg.numero2;
+		aux = insereEspaco(reg.numero2, reg.tamanhoNumero2);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//CEP 2
+		cout << "CEP 2: ";
+		cin >> reg.CEP2;
+		aux = insereEspaco(reg.CEP2, reg.tamanhoCEP2);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Telefone 2
+		cout << "Telefone 2: ";
+		cin >> reg.telefone2;
+		aux = insereEspaco(reg.telefone2, reg.tamanhoTelefone2);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Curso
+		cout << "Curso: ";
+		cin >> reg.curso;
+		aux = insereEspaco(reg.curso, reg.tamanhoCurso);
+		strcat(registro, aux);
+		strcat(registro, " ");
+		//Ano Ingresso 
+		cout << "Ano Ingresso: ";
+		cin >> reg.anoIngresso;
+		aux = insereEspaco(reg.anoIngresso, reg.tamanhoAnoIngresso);
+		strcat(registro, aux);
+
+		//coloca o xA no fim registro ao inves do \0
+		registro[241] = '\xA'; 
+		
+		//insere na ultima linha do arquivo
+		f.seekg(n*sizeof(registro), ios::beg);
+		f.clear();
+		f.write(registro, 242);		
+			
+	}
+	
+	/*void escreve()
+	{
+		busca na fila posicao livre
+		f.clear();
+		f.seekg(posicao,ios::beg);
+		f.write(nome, sizeof(Registro));
+		
+	}
+	*/
+	
+	
 	
 	void setEnd(int endereco)
 	{
