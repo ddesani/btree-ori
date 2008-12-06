5cb72792778506ce5bfca338dec91207c3c80039
victorbrsantos <victorbrsantos@53171014-be3d-11dd-824f-6f77523b18d3>
2008-12-06 02:19:23 +0000

    Classe Chave com sets e gets

--- trunk/BTree/src/Chave.h
+++ trunk/BTree/src/Chave.h
@@ -6,9 +6,31 @@ using namespace std;
 // definicao da struct key
 class Chave
 {
-public:
+private:
 	long registro; // posicao da chave no vetor de registros
 	int valor; // valor da chave
+public:
+	
+	long getRegistro()
+	{
+		return registro;
+	}
+	
+	void setRegistro(long reg)
+	{    
+		registro = reg;
+	}
+	
+	int getValor()
+	{
+		return valor;
+	}
+	
+	void setValor(int val)
+	{
+		valor = val;
+	}
+	
 };
 
 #endif /*CHAVE_H_*/
