6c308fa0cb3f0268763ba0e62f124c9b5974ada9
julianaseven <julianaseven@53171014-be3d-11dd-824f-6f77523b18d3>
2008-12-06 01:47:14 +0000

    nova classe ArquivoB usando arquivos binarios e comentada em java doc

--- BTree/src/ArquivoB.h
+++ BTree/src/ArquivoB.h
@@ -1,365 +1,123 @@
 #ifndef ARQUIVOB_H_
 #define ARQUIVOB_H_
+
+#include "No.h"
 using namespace std;
 
-class ArquivoB
-{
+/* identificadores para os enderecos no arquivo da arvore */
+#define POSICAO_IDENTIFICADOR 0
+#define POSICAO_ORDEM 1
+#define POSICAO_RAIZ 5
+#define POSICAO_DISPONIVEIS 9
+
+
+/**
+ * @brief Classe ArquivoB
+ * Classe para manipular o arquivo da arvore B. La encontra-se os nos que serao
+ * gravados as chaves, ponteiros, enderecos, e outros dados.
+ *
+ * @author Juliana Scaquetti, 298921
+ * @author Victor Hugo B. R. Santos, 298557
+ * @author Diego D. da Silva, 298700
+ * @author Thiago A. C. Chagas, 280615
+ * @version 5 de Dezembro de 2008
+ */
+class ArquivoB {
 private:
-	string filename;
+	/**
+	 * Stream para controlar o arquivo.
+	 */
 	fstream fB;
+
+	/**
+	 * Nodo da arvore B.
+	 */
+	No nodo;
+
+	/**
+	 * Ordem da arvore B.
+	 */
+	int ordem;
+
 
 public:
 
-	/*
-	 * Ctor para ArquivoB
-	 * 	inicializa filename
+	/**
+	 * Construtor
+	 * Abre o arquivo da arvore.
 	 */
-	ArquivoB()
-	{
-		filename = "";
-
+	ArquivoB(){
+		fB.open("btree.txt", fstream::in | fstream::out | fstream::binary);
+		this->ordem = this->getOrdem();
+		nodo.iniciaNo(this->ordem);
 	}
 
-	/*
-	 * Dtor para ArquivoB
-	 * 	fecha o arquivo caso este esteja aberto
+	/**
+	 * Destrutor
+	 * Fecha o arquivo da arvore.
 	 */
-	~ArquivoB()
-	{
-		if (fB.is_open())
+	~ArquivoB(){
+		if(fB.is_open())
 			fB.close();
 	}
 
-	/*
-	 * Metodo para abertura do arquivo
-	 * 	retorna 
+	/**
+	 * Metodo para construir o cabecalho do arquivo da arvore.
+	 * @param n ordem da arvore.
 	 */
-
-	void setCabecalho(int n)
-	{
-		if (fB.is_open())
-		{
-			fB.seekg(0);
-			fB << 'I' << '.' << n << '.' << 0 << '.' << 0 << '\xa';
-		}
+	void setCabecalho(int n){
+		char identificador = 'I';
+		long temp = 0;
+		fB.seekp(0, ios::beg);
+		fB.write(reinterpret_cast<char*>(&identificador), sizeof(identificador));
+		fB.write(reinterpret_cast<char*>(&n), sizeof(n));
+		fB.write(reinterpret_cast<char*>(&temp), sizeof(temp));
+		fB.write(reinterpret_cast<char*>(&temp), sizeof(n));
 	}
 
-	bool open()
-	{
-		if (filename.size() > 0 && !fB.is_open())
-			fB.open(filename.c_str());
-
-		return fB.is_open();
-	}
-
-	/*
-	 * Metodo para inicializar filename
+	/**
+	 * Metodo para mudar o valor da raiz.
+	 * @param endereco endereco que vai ser colocado no campo da raiz no cabecalho do arquivo da arvore.
 	 */
-	void setFileName(string nome)
-	{
-		filename = nome;
+	void setRaiz(long endereco){
+		fB.seekp(POSICAO_RAIZ, ios::beg);
+		fB.write(reinterpret_cast<char*>(&endereco), sizeof(endereco));
 	}
 
-	/*
-	 * Metodo para obter filename
+	/**
+	 * Metodo que escreve o no carregado no arquivo da arvore.
+	 * @param nodo no carregado na memoria para ser escrito no arquivo da arvore.
 	 */
-	string getFileName()
-	{
-		return filename;
+	void escreveNo(const No &nodo){
+		fB.seekp(nodo.get_pos(), ios::beg);
+		fB.write(reinterpret_cast<char*>(&nodo->getNChaves()), 4);
+		fB.write(reinterpret_cast<char*>(&nodo->get_pai()), 4);
+		for (i = 0; i < this->ordem; ++i) {
+														//mudar com set e get
+			fB.write(reinterpret_cast<char*>(&nodo->chave[i]->valor), 4);
+			fB.write(reinterpret_cast<char*>(&nodo->chave[i]->registro), 4);
+			fB.write(reinterpret_cast<char*>(&nodo->_filho[i]), 4);
+		}
 	}
 
-	/*
-	 * Funcoes para obter o Nodo
+	/**
+	 * Metodo que retorna o endereco da lista de disponiveis do arquivo da arvore.
+	 * @return retorna o endereco que esta disponivel para gravar um nodo no arquivo da arvore.
 	 */
-
-	long getFilhoN(long posicao, int indice)
-	{
-		int ordem = getOrdemC();
-		if (indice < ordem)
-		{
-			int i=0;
-			char a[1];
-			long end;
-			fB.seekg(posicao, ios::beg);
-			do
-			{
-				fB >> a[0];
-
-				if (a[0] == '.')
-					i++;
-			} while (i != indice + 2*(ordem-1) + 2); //posicao inicial dos filhos no No
-
-			fB >> end;
-
-			return end;
-
-		}
-		else
-			return -1; //mudar para 0 quando testar com registro.txt
+	long getListaDisp(){
+		long enderecoDisp;
+		fB.seekg(POSICAO_DISPONIVEIS, ios::beg);
+		fB.read(reinterpret_cast<char*>(&enderecoDisp), sizeof(enderecoDisp));
+		return enderecoDisp;
 	}
 
-	long getPaiN(long posicao)
-	{
-		if (posicao != 0) //ver o limite para a posicao de acordo com o cabecalho
-		{
-			int i=0;
-			char a[1];
-			long end;
-			int nChavesNo;
-			fB.clear();
-			fB.seekg(posicao, ios::beg);
-			fB >> nChavesNo;
-			fB.clear();
-			fB.seekg(posicao, ios::beg);
-
-			do
-			{
-				fB >> a[0];
-
-				if (a[0] == '.')
-					i++;
-			} while (i != 3*nChavesNo);
-
-			fB >> end;
-
-			return end;
-
-		}
-		else
-			return -1; //mudar para 0 quando testar com registro.txt
-	}
-
-	long getEndN(long posicao, int indice)
-	{
-		int ordem = getOrdemC();
-		if (indice < ordem -1)
-		{
-			int i=0;
-			char a[1];
-			long end;
-			fB.seekg(posicao, ios::beg);
-			do
-			{
-				fB >> a[0];
-
-				if (a[0] == '.')
-					i++;
-			} while (i != indice + ordem);
-
-			fB >> end;
-
-			return end;
-
-		}
-		else
-			return -1; //mudar para 0 quando testar com registro.txt
-	}
-
-	int getRaN(long posicao, int indice)
-	{
-		if (indice < getOrdemC() -1)
-		{
-			int i=0;
-			char a[1];
-			int ra;
-			fB.seekg(posicao, ios::beg);
-			do
-			{
-				fB >> a[0];
-
-				if (a[0] == '.')
-					i++;
-			} while (i != indice+1);
-
-			fB >> ra;
-
-			return ra;
-
-		}
-		else
-			return -1; //mudar para 0 quando testar com registro.txt
-	}
-
-	int getNumeroChavesN(long posicao)
-	{
-		int nChavesN =0;
-
-		fB.seekg(posicao, ios::beg);
-		fB >> nChavesN;
-
-		return nChavesN;
-
-	}
-
-	void setNumeroChavesN(long posicao)
-	{
-		int nChavesN =0;
-
-		fB.seekp(posicao, ios::beg);
-		fB << nChavesN;
-
-	}
-
-	/*
-	 * Funcoes para obter o cabecalho
+	/**
+	 * Metodo que muda o valor do endereco na lista de disponiveis no arquivo da arvore.
+	 * @param endereco endereco a ser gravado na lista de disponiveis do arquivo da arvore.
 	 */
-
-	char getIdC()
-	{
-		char id[1];
-
-		fB.seekg(0, ios::beg);
-		fB >> id[0];
-
-		return id[0];
-	}
-
-	int getOrdemC()
-	{
-		int ordem;
-
-		fB.flush();
-		fB.seekg(2, ios::beg);
-		fB >> ordem;
-		cout << fB.tellg();
-
-		return ordem;
-	}
-
-	int getEndRaizC()
-	{
-		int endRaiz;
-
-		fB.seekg(4, ios::beg);
-		fB >> endRaiz;
-
-		return endRaiz;
-	}
-	
-	void setRaiz(int endRaiz)
-	{
-		if (fB.is_open())
-		{
-			fB.seekp(4, ios::beg);
-			fB << endRaiz;
-		}
-	}
-
-	int getEndListaC()
-	{
-		int endListaDispo;
-		char a[1];
-		fB.seekg(4, ios::beg);
-
-		do
-		{
-			fB >> a[0];
-		} while (a[0] != '.');
-
-		fB >> endListaDispo;
-		return endListaDispo;
-	}
-	void escreveNo(No* no, int nChaves)
-	{
-		int endRaiz;
-		int pos;
-
-		fB.seekp(0, ios::end);
-
-		pos = fB.tellp();
-
-		fB << no->getNChaves() << '.';
-		for (int i=0; i<nChaves; ++i)
-			fB << no->chave[0].valor << '.';
-
-		for (int i=0; i<nChaves; ++i)
-			fB << no->chave[0].registro << '.';
-
-		fB << no->pai << '.';
-		for (int i = 0; i<= nChaves; ++i)
-			//escreve n+1 (ordem) ponteiros
-			fB << no->filho[i] << '.';
-
-		fB << '\xa'; // termina o nodo com \xa
-
-		endRaiz = getEndRaizC();
-
-		if (endRaiz == 0)
-		{
-			fB.seekp(4);
-			fB << pos;
-		}
-
-	}
-
-	No carregaNo(long posicao, int nChaves)
-	{
-		No aux(nChaves);
-		if (posicao != 0)
-		{
-
-			aux.nChaves = getNumeroChavesN(posicao);
-			for (int i=0; i<nChaves; ++i)
-				aux.chave[i].valor = getRaN(posicao, i);
-
-			for (int j=0; j<nChaves; ++j)
-				aux.chave[j].registro = getEndN(posicao, j);
-
-			aux.set_pai(getPaiN(posicao));
-
-			for (int k=0; k<nChaves+1; ++k)
-				aux.set_filho(getFilhoN(posicao, k), k);
-			aux.set_pos(posicao);
-		}
-
-		return aux;
-	}
-
-	void escreveNo2(No* no)
-	{
-		int endRaiz;
-		int pos;
-
-		if (no->get_pos() == 0)
-		{
-			fB.clear();
-			fB.seekp(no->get_pos(), ios::end);
-
-			pos = fB.tellp();
-			endRaiz = getEndRaizC();
-
-			if (endRaiz == 0) //atualizando o end da raiz
-			{
-
-				fB.seekp(4);
-				fB << pos;
-				fB.seekp(0, ios::end);
-			}
-
-			fB << no->getNChaves() << '.';
-			for (int i=0; i<no->getNChaves(); ++i)
-				fB << no->chave[i].valor << '.';
-
-			for (int i=0; i<no->getNChaves(); ++i)
-				fB << no->chave[i].registro << '.';
-
-			fB << no->_pai << '.';
-			for (int i = 0; i<= no->getNChaves(); ++i)
-				fB << no->_filho[i] << '.'; //escreve n+1 (ordem) ponteiros
-
-
-			fB << '\xa'; // termina o nodo com \xa
-
-			/*endRaiz = getEndRaizC();
-
-			 if (endRaiz == 0)
-			 {
-			 fB.seekp(4);
-			 fB << pos;
-			 }*/
-
-		}
-
+	void setListaDisp(const long endereco){
+		fB.seekp(POSICAO_DISPONIVEIS, ios::beg);
+		fB.write(reinterpret_cast<char*>(&endereco), sizeof(endereco));
 	}
 
 };
